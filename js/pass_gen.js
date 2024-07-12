9ef3ea018298704feb3c2805a9801c7ff9199f58
megaoptimal <alejandroforero65@gmail.com>
2024-07-12 13:33:41 -0500

    Changed input handling for password length to non-regex solution

--- js/pass_gen.js
+++ js/pass_gen.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function (e) {
     NumberInput = document.getElementById("pswlength");
     NumberInput.addEventListener("keydown", isNumberKeyorEnter);
+    NumberInput.addEventListener("change", isNumberKeyorEnter);
 })
 
 function isNumberKeyorEnter(evt) {
@@ -8,7 +9,8 @@ function isNumberKeyorEnter(evt) {
         evt.preventDefault();
         return false;
     }
-    var newValue = this.value.replace(new RegExp(/[^\d]/, 'ig'), "");
+    var allowedChars = "123456789";
+    var newValue = Array.from(this.value).filter(f => allowedChars.includes(f)).join('');
     this.value = newValue;
 };
 
